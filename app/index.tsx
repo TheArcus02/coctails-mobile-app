214022391544300ed57a00419c128d06fe0f1e2c
TheArcus02 <m.urbaniak.inf@gmail.com>
2025-04-12 17:10:05 +0200

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className='text-center'>
+      <Text className='text-3xl font-semibold'>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
